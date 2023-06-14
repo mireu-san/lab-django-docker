747a71918cd49973bb71f040a067dff83b1bc185
mireu-san <starmireu@gmail.com>
2023-06-14 13:22:15 +0900

    fixing github actions v1

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,6 +7,7 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
+    
 
     def test_add_numbers(self):
         """Test adding numbers together"""
@@ -19,3 +20,4 @@ class CalcTests(SimpleTestCase):
         res = calc.subtract(10, 15)
 
         self.assertEqual(res, 5)
+        
