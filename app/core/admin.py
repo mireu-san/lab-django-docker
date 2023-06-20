77980ddedc145b565030e207aaae6d02c656c3df
mireu-san <starmireu@gmail.com>
2023-06-20 22:24:34 +0900

    admin - change user

--- app/core/admin.py
+++ app/core/admin.py
@@ -13,7 +13,21 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ["id"]
     list_display = ["email", "name"]
-    fieldsets = ((None, {"fields": ("email", "password")}), ())
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
 
 
 admin.site.register(models.User, UserAdmin)
