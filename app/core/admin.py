10aa86b5d471fe34d64c479aa357dbae72f3c093
mireu-san <starmireu@gmail.com>
2023-06-20 22:18:27 +0900

    add translation utils

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customisation
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -12,6 +13,7 @@ class UserAdmin(BaseUserAdmin):
 
     ordering = ["id"]
     list_display = ["email", "name"]
+    fieldsets = ((None, {"fields": ("email", "password")}), ())
 
 
 admin.site.register(models.User, UserAdmin)
