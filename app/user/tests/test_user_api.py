cd60de9dd3d6919453ddc2f08d40480fe4338c03
mireu-san <starmireu@gmail.com>
2023-06-30 19:53:06 +0900

    test_user_api - Auth, token not found OK

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -80,3 +80,21 @@ class PublicUseApiTests(TestCase):
 
         self.assertIn("token", res.data)
         self.assert_Equal(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_bad_credentials(self):
+        """test returns error if credentials invalid"""
+        create_user(email="test@example.com", password="goodpass")
+
+        payload = {"email": "test@example.com", "password": "badpass"}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_blank_password(self):
+        """Test postig a blank password returns an error"""
+        payload = {"email": "test@example.com", "password": ""}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn("token", res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
